afdebf0d3fd3883b09cead4f10963556fbf26875
Gowri Gows <96278005+GOWRIBM57@users.noreply.github.com>
2022-05-16 16:42:28 +0000

    python07

--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -10,7 +10,7 @@ while True:
         largest = num if largest < num or largest == None else largest
         smallest = num if smallest > num or smallest == None else smallest
     except:
-        print "Invalid input"
+        print ('Invalid input')
 
 print "Maximum number is ", largest
 print "Minimum number is ", smallest
