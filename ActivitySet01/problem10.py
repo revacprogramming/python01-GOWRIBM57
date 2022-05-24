d760c7206605f81564a06147da2651338f390c3e
Gowri Gows <96278005+GOWRIBM57@users.noreply.github.com>
2022-05-24 16:56:50 +0000

    python10

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,4 +1,13 @@
-filename = "dataset/mbox-short.txt"
-
-
-
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+  words=line.split()
+  for test in words:
+    if test not in lst:
+      lst.append(test)
+    else:
+      continue
+#print(line.strip())
+lst.sort()
+print(lst)
