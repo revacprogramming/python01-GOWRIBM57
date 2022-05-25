d52a64dfe90c67569da9209fce404aa166a27237
Gowri Gows <96278005+GOWRIBM57@users.noreply.github.com>
2022-05-25 04:37:40 +0000

    python10

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -2,12 +2,13 @@ fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
-  words=line.split()
-  for test in words:
-    if test not in lst:
-      lst.append(test)
-    else:
-      continue
-#print(line.strip())
+    word = line.rstrip().split()
+    for element in word:
+        if element in lst:
+            continue
+        else:
+            lst.append(element)
 lst.sort()
 print(lst)
+            
+    
--- ActivitySet01/problem11.py
+++ ActivitySet01/problem11.py
@@ -3,3 +3,21 @@
 filename = "dataset/mbox-short.txt"
 
 
+fname = input("Enter file name: ")
+fh = open(fname)
+count = dict()
+for line in fh:
+    if line.startswith("From"):
+        words=line.split()
+        test=words[1]
+        for test in words:
+            if test in count:
+                count[test] = count[test] + 1
+            else:
+                count[test] = 1
+        
+#print(line.strip())
+for k,v in count.items():
+    print(k,'-',v)
+#print(count.keys())
+#print(count.values())
